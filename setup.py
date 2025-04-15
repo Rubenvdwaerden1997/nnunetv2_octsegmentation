9228b191e2a3bdf3ecfd9d931abed46f13618da2
Rubenvanderwaerden1997 <rubenvdwaerden@hotmail.com>
2025-04-15 09:23:42 +0200

    Update files

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nnunetv2_octsegmentation',
+    name='nnunetv2',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=["nnunetv2", "nnunetv2.*"]),
     install_requires=[],
-    entry_points={'console_scripts': [
-        'nnUNet_predict = nnunetv2.inference.predict_from_raw_data:predict_entry_point',
+    entry_points={
+        'console_scripts': [
+            'nnUNet_predict = nnunetv2.inference.predict_from_raw_data:predict_entry_point',
         ],
     }
 )
