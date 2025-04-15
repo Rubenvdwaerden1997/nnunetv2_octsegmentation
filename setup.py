b85fdf37db7741166ec9f0a3b5e603ad5d4561bd
Rubenvanderwaerden1997 <rubenvdwaerden@hotmail.com>
2025-04-15 07:43:04 +0200

    update files

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[],
+    entry_points={'console_scripts': [
+        'nnUNet_predict = nnunet.inference.predict:predict_entry_point',
+        ],
+    }
 )
